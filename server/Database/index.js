f0189bb2f0306f60d737f22a9d1123d5ab5c040b
abed-404 <asafeiya2018@gmail.com>
2022-08-17 20:18:17 +0300

    file structure

--- server/Database/index.js
+++ server/Database/index.js
@@ -1,13 +1,8 @@
-// const fs = require('fs');
 const path = require('path');
 const fs = require('fs').promises;
 
 const filePath = path.join(__dirname, 'books.json');
 
-// async function readJson() {
-//   const data = await fs.readFile(filePath, 'utf8');
-//   return JSON.parse(data);
-// }
 function readJson() {
   return fs.readFile(filePath, 'utf8');
 }
